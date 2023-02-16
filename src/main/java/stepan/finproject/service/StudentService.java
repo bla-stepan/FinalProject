2f91a73f96a78f4c460b95bea95733fa75e14bc7
Stepan <bla-stepan@mail.ru>
2023-02-16 11:35:12 +0300

    убраны все аннотации контроллера, добавлена аннотация сервис

--- src/main/java/stepan/finproject/service/StudentService.java
+++ src/main/java/stepan/finproject/service/StudentService.java
@@ -1,34 +1,25 @@
 package stepan.finproject.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import stepan.finproject.model.Student;
 import stepan.finproject.repository.StudentRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/student")
+@Service
 public class StudentService {
 
-    @Autowired
     StudentRepository studentRepository;
 
-    @PostMapping("/api/student/save")
     public Student save(Student student){
         return studentRepository.save(student);
     }
 
-    @GetMapping("/api/student/findById")
     public Optional<Student> findById(Integer id){
         return studentRepository.findById(id);
     }
 
-    @GetMapping("/api/student/findAll")
     public List<Student> findAll(){
         List<Student> students = studentRepository.findAll();
         return students;
