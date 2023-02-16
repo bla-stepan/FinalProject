7969559c64b4d663518d69fa07f913759677eb97
Stepan <bla-stepan@mail.ru>
2023-02-16 13:49:48 +0300

    скорректирован StudentService добавлено внедрение зависимости

--- src/main/java/stepan/finproject/service/StudentService.java
+++ src/main/java/stepan/finproject/service/StudentService.java
@@ -1,16 +1,26 @@
 package stepan.finproject.service;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Service;
 import stepan.finproject.model.Student;
 import stepan.finproject.repository.StudentRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 @Service
 public class StudentService {
 
-    StudentRepository studentRepository;
+    private StudentRepository studentRepository;
+
+    public StudentService(final StudentRepository studentRepo) {
+        studentRepository = studentRepo;
+    }
 
     public Student save(Student student){
         return studentRepository.save(student);
