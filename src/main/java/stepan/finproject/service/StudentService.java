d517bf90c69c57d68b9d42e1e4eac482fe0d44e6
Stepan <bla-stepan@mail.ru>
2023-02-16 10:59:51 +0300

    добавлен класс StudentService c методами save, findById, findAll

---
+++ src/main/java/stepan/finproject/service/StudentService.java
@@ -0,0 +1,36 @@
+package stepan.finproject.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import stepan.finproject.model.Student;
+import stepan.finproject.repository.StudentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    @PostMapping("/api/student/save")
+    public Student save(Student student){
+        return studentRepository.save(student);
+    }
+
+    @GetMapping("/api/student/findById")
+    public Optional<Student> findById(Integer id){
+        return studentRepository.findById(id);
+    }
+
+    @GetMapping("/api/student/findAll")
+    public List<Student> findAll(){
+        List<Student> students = studentRepository.findAll();
+        return students;
+    }
+}
